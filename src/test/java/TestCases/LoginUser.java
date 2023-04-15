7d95dde0cf2e4418950f2afaee7230cdffaec09c
davidlouistandio <davidlouistandio@gmail.com>
2023-04-15 17:20:44 +0800

    Change pom

--- src/test/java/TestCases/LoginUser.java
+++ src/test/java/TestCases/LoginUser.java
@@ -2,19 +2,32 @@ package TestCases;
 
 import PageObjects.ProductPage;
 import Utils.BaseTest;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Epic("Swag Labs")
+@Feature("Login")
 public class LoginUser extends BaseTest {
 
-    @Test
+    @Story("Login valid user")
+    @Link(name = "allure", type = "mylink")
+    @Issue("1")
+    @TmsLink("TMS Link")
+    @Description("Login User with valid id and password")
+    @Test( description = "Login valid user")
     public void loginValidUser(){
         ProductPage productPage = loginPage.loginUser("standard_user","secret_sauce");
         String headerText = productPage.getHeaderText();
         Assert.assertEquals(headerText, "Products");
     }
 
-    @Test
+    @Story("Login invalid user")
+    @Link(name = "allure", type = "mylink")
+    @Issue("1")
+    @TmsLink("TMS Link")
+    @Description("Login User with invalid id and password")
+    @Test( description = "Login invalid user")
     public void loginInvalidUser(){
         loginPage.loginUser("standard_user","invalid");
         String alertText = loginPage.getAlertText();
