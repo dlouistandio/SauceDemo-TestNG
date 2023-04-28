12c3291154349efc24d4dd6c9fa5f6c8e11ee50b
davidlouistandio <davidlouistandio@gmail.com>
2023-04-28 21:51:00 +0800

    Try selenium grid

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -7,11 +7,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -23,19 +27,25 @@ public class BaseTest{
 
     public WebDriver initDriver() throws IOException {
         Properties props = new Properties();
+        DesiredCapabilities caps = new DesiredCapabilities();
+        WebDriver driver = null;
+
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/GlobalData.properties");
         props.load(fis);
         String browserName = props.getProperty("browser");
         String webUrl = props.getProperty("url");
 
         if (browserName.equalsIgnoreCase("chrome")){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+//            WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver();
         }
         if (browserName.equalsIgnoreCase("firefox")){
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+//            WebDriverManager.firefoxdriver().setup();
+//            driver = new FirefoxDriver();
+            caps.setCapability(CapabilityType.BROWSER_NAME, "firefox");
         }
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(webUrl);
